55c7af0a2972f84f5f9e11577aafa98a18bf7c41
richard <richard.deroure@gmail.com>
2020-05-14 11:45:59 +0100

    added simple api call to display all entities in database name column

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import HomePage from './components/HomePage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1>My Recipes App</h1>
+        <HomePage />
       </header>
     </div>
   );
